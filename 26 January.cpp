0823e02d8655818be9c4a1ae05e5d2a05960234b
Hiren Bambhaniya <148859956+HirenCodeMaster11@users.noreply.github.com>
2024-01-28 21:13:43 +0530

    Add files via upload

---
+++ 26 January.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+main()
+{
+	cout << "26 January 2024" << endl<<endl<<endl;
+	
+	cout <<"-> India is commemorating its 75th Republic Day on 26 January 2024.Being the 75th anniversary of Republic Day, it is going to be an additional special day."<< endl<<endl;
+	cout <<"-> Republic Day marks the adoption of India's constitution and the country's transition to a republic on January 26, 1950. Every year, the celebrations marking the day feature spectacular military and cultural pageantry. In New Delhi, armed forces personnel march along the Kartavya Path in an elaborate display of military might. The epic show on Kartavya Path eclipses everything else happening across the country on this auspicious day."
+<< endl<<endl;
+	cout <<"-> The celebrations, inaugurated with a grand parade, are held in the capital, New Delhi, from Raisina Hill near the Rashtrapati Bhavan (President's House), along the Kartavya Path, past India Gate, and onto the historic Red Fort. On this day, ceremonious parades take place at the Kartavya Path, which is performed as a tribute to India, its unity in diversity, and its rich cultural heritage by the states of India by building beautiful tableaus."
+<<endl;
+}
